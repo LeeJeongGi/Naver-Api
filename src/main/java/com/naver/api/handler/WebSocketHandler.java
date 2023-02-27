fb20fbb0412800a870e4346fbbed5ab5a2ffecbc
LeeJeongGi <apple4rhk@naver.com>
2023-02-27 23:09:22 +0900

    refactor : socket session 관리

--- src/main/java/com/naver/api/handler/WebSocketHandler.java
+++ src/main/java/com/naver/api/handler/WebSocketHandler.java
@@ -6,16 +6,28 @@ import com.naver.api.dto.ChatRoom;
 import com.naver.api.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 @RequiredArgsConstructor
 public class WebSocketHandler extends TextWebSocketHandler {
 
     private final ObjectMapper objectMapper;
+
     private final ChatService chatService;
+
+    private final Set<WebSocketSession> sessions = ConcurrentHashMap.newKeySet();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
@@ -24,5 +36,10 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
         ChatRoom chatRoom = chatService.findRoomById(chatMessage.getRoomId());
         chatRoom.handlerActions(session, chatMessage, chatService);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
     }
 }
