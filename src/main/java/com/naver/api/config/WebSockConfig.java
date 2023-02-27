0bb8bddbbb03ac3f5a530a766c49b35eb2c543ea
LeeJeongGi <apple4rhk@naver.com>
2023-02-27 23:09:34 +0900

    refactor : sockjs 추가

--- src/main/java/com/naver/api/config/WebSockConfig.java
+++ src/main/java/com/naver/api/config/WebSockConfig.java
@@ -16,6 +16,8 @@ public class WebSockConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "ws/chat").setAllowedOrigins("*");
+        registry.addHandler(webSocketHandler, "ws/chat")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 }
